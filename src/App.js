f571b6927e3629084b43703a15fda135712c179b
Pipa35 <66171653+Pipa35@users.noreply.github.com>
2020-09-13 09:46:55 +0100

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/filipa-pinho-2a7a322a/"
+          target="_blank"
+        >
+          Filipa Pinho
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Pipa35/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
