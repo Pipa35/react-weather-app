aaeca6cd87d3257714a2cb65ce5b8759ddb2faf3
Pipa35 <66171653+Pipa35@users.noreply.github.com>
2020-09-13 10:26:45 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/filipa-pinho-2a7a322a/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Filipa Pinho
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Pipa35/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/filipa-pinho-2a7a322a/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Filipa Pinho
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Pipa35/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
